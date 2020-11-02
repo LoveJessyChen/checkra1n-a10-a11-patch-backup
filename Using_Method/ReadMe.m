7dc8e1f884db2b5c4c669c46b4a4c22595b9979d
LoveJessyChen <48858531+LoveJessyChen@users.noreply.github.com>
2020-11-02 11:37:13 +0800

    Create ReadMe.m

---
+++ Using_Method/ReadMe.m
@@ -0,0 +1,15 @@
+# checkra1n_A11_A10_mac_Patch命令行文件的说明
+
+
+
+文件提权
+
+
+chmod -R 777 checkra1n_A11_A10_mac_Patch.command
+
+后边的文件替换为完整路径
+
+
+/Applications/checkra1n.app/Contents/MacOS/checkra1n-patch -c
+
+-c进入命令行模式
